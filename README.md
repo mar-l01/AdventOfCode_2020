bd2b800dc9ea0391a33ac8c7ae70f9f1f471b441
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-12-04 18:15:35 +0100

    add code to script for computing solution for puzzle 2 of day 4

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Code to solve the puzzles of Advent of Code 2020
     - [Puzzle 2](https://adventofcode.com/2020/day/3#part2): Find the total number of trees (multiplied) encountered when flying through a map of '.' (:= no tree) and '#' (:= tree) with different slopes  
 - Day 4  
     - [Puzzle 1](https://adventofcode.com/2020/day/4): Find the total number of valid passports given a file of passports as dictionaries  
+    - [Puzzle 2](https://adventofcode.com/2020/day/4#part2): Find the total number of valid passports with all entries holding valid values    
--- day_4/puzzles.py
+++ day_4/puzzles.py
@@ -1,3 +1,5 @@
+import re
+
 PASSPORTS_FILE = "passports.txt"
 
 PASSPORT_ENTRIES_DELIMITER = ' '
@@ -39,34 +41,189 @@ def get_list_of_passports():
 
     return list_of_passports  
 
-def get_number_of_valid_passports(list_of_passports):
+# -------------------------- Puzzle 1 --------------------------
+
+def get_passports_with_required_fields(list_of_passports):
     """
-    Go through given list of passports (dictionaries) and count all passports which contain
-    all required fields. Only "cid" field is optional.
+    Go through given list of passports (dictionaries) and get all passports which contain
+    all required fields. Only "cid" field is optional. Add them to a list and return it.
     """
-    nb_of_valid_passports = 0
-    
+    list_of_valid_passports = []
+
     for passport in list_of_passports:
-        fields_of_current_passport = list(passport.keys())
-        required_fields_present = True
+        if all_required_fields_present(passport):
+            list_of_valid_passports.append(passport)
 
-        # check if above list of keys of the current passport contains all required keys
-        for req_field in REQUIRED_FIELDS:
-            if not req_field in fields_of_current_passport:
-                required_fields_present = False
-                break
+    return list_of_valid_passports
 
-        if required_fields_present:
-            nb_of_valid_passports += 1
+def all_required_fields_present(passport):
+    """ Return True if given passport contains all required fields """
+    fields_of_current_passport = list(passport.keys())
+    required_fields_present = True
 
-    return nb_of_valid_passports
+    # check if above list of keys of the current passport contains all required keys
+    for req_field in REQUIRED_FIELDS:
+        if not req_field in fields_of_current_passport:
+            required_fields_present = False
+            break
+
+    return required_fields_present
+
+# -------------------------- Puzzle 2 --------------------------
+
+def get_passports_with_valid_entries(list_of_valid_passports):
+    """
+    Given a list of valid passports (all required fields present), go through all
+    of them and check if each field holds a valid entry.
+    """
+    list_of_passports_with_valid_entries = []
+
+    for passport in list_of_valid_passports:
+        if all_passport_entries_valid(passport):
+            list_of_passports_with_valid_entries.append(passport)
+
+    return list_of_passports_with_valid_entries
+
+def all_passport_entries_valid(passport):
+    """
+    Check if all fields in given passport hold valid values.
+    Note: We assume here that only valid passports (:= all required fields present)
+    are given.
+    """
+    all_entries_valid = True
+
+    # check if each entry of required fields holds a valid value
+    for passport_entry in passport.items():
+        if not is_passport_entry_valid(passport_entry):
+            all_entries_valid = False
+            break
+
+    return all_entries_valid
+    
+def is_passport_entry_valid(passport_entry):
+    """ Check if given passport entry (key, value) is valid """
+    pp_key = passport_entry[0]
+    pp_value = passport_entry[1]
+    is_pp_entry_valid = False
+
+    if pp_key == "byr":
+        is_pp_entry_valid = is_year_valid(pp_value, 1920, 2002)
+    elif pp_key == "iyr":
+        is_pp_entry_valid = is_year_valid(pp_value, 2010, 2020)
+    elif pp_key == "eyr":
+        is_pp_entry_valid = is_year_valid(pp_value, 2020, 2030)
+    elif pp_key == "hgt":
+        is_pp_entry_valid = is_height_valid(pp_value)
+    elif pp_key == "hcl":
+        is_pp_entry_valid = is_hcl_valid(pp_value)
+    elif pp_key == "ecl":
+        is_pp_entry_valid = is_ecl_valid(pp_value)
+    elif pp_key == "pid":
+        is_pp_entry_valid = is_pid_valid(pp_value)
+    elif pp_key == "cid":
+        is_pp_entry_valid = True # cid is optional, thus ignored
+
+    return is_pp_entry_valid
+
+def is_number_valid(number_str, number_min, number_max):
+    """ Return true if number (given as str) is within given range and convertible to an integer """
+    is_nb_valid = False
+
+    # make sure string is convertible to an integer
+    try:
+        number = int(number_str)
+        if number_min <= number <= number_max:
+            is_nb_valid = True
+    except:
+        print("[!] Could not parse given string: {})".format(pp_value))
+
+    return is_nb_valid
+
+def is_year_valid(str_year, year_min, year_max):
+    """ Check if given year (as str) is valid under given conditions (:= min, max value) """
+    is_year_valid = False
+
+    # year has to be given as 4 digits
+    if len(str_year) == 4:
+        is_year_valid = is_number_valid(str_year, year_min, year_max)
+        
+    return is_year_valid
+
+def is_height_valid(str_height):
+    """
+    Check if given height (as str) is valid under given conditions:
+    - if 'cm': height at least 150 and at most 193
+    - if 'in': height must be at least 59 and at most 76
+
+    Note: str_height is given as 192cm or 70in assuming that the last two
+    characters denote the unit and the first characters until the second to
+    last denote the height value.
+    """
+    is_height_valid = False
+
+    # make sure string is long enough (expect at least 1 number and 2 chars for 'cm' or 'in')
+    if len(str_height) <= 3:
+        return is_height_valid
+
+    # expect the first characters until the second to last to determine the height
+    height = str_height[:-2]        
+    
+    # expect the last two characters to determine the height unit
+    height_unit = str_height[-2:]
+
+    if height_unit == "cm":
+        is_height_valid = is_number_valid(height, 150, 193)
+    elif height_unit == "in":
+        is_height_valid = is_number_valid(height, 59, 76)
+
+    return is_height_valid
+
+def is_hcl_valid(str_hcl):
+    """
+    Check if given hair color (as str) is valid under given conditions:
+    - '#' followed by exactly six characters (0-9 or a-f)
+    """
+    is_hcl_valid = False
+    expected_length = 7
+    pattern = "(^#)[0-9, a-f]{" + str(expected_length - 1) + "}"
+
+    if len(str_hcl) == expected_length and re.match(pattern, str_hcl):
+        is_hcl_valid = True
+
+    return is_hcl_valid 
+
+def is_ecl_valid(str_ecl):
+    """
+    Check if given eye color (as str) is valid under given conditions:
+    - exactly one of: amb blu brn gry grn hzl oth
+    """
+    return str_ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+def is_pid_valid(str_pid):
+    """
+    Check if given pid (as str) is valid under given conditions:
+    - a nine-digit number, including leading zeroes
+    """
+    is_pid_valid = False
+    expected_length = 9
+    pattern = "[0-9]{" + str(expected_length) + "}"
+
+    if len(str_pid) == expected_length and re.match(pattern, str_pid):
+        is_pid_valid = True
+
+    return is_pid_valid
+
+# -------------------------- Solution of puzzles 1 and 2 --------------------------
 
 def compute_solution_of_puzzle():
     """ Find the total number of valid passports """
     list_of_passports = get_list_of_passports()
-    nb_of_valid_passports = get_number_of_valid_passports(list_of_passports)
+    list_of_valid_passports = get_passports_with_required_fields(list_of_passports)
 
-    print("[+] Solution of day4/puzzle1: {} passports are valid".format(nb_of_valid_passports))
+    print("[+] Solution of day4/puzzle1: {} passports are valid".format(len(list_of_valid_passports)))
 
+    list_of_passports_with_valid_entries = get_passports_with_valid_entries(list_of_valid_passports)
+    print("[+] Solution of day4/puzzle2: {} passports have all entries containing valid values"\
+          .format(len(list_of_passports_with_valid_entries)))
 if __name__ == "__main__":
     compute_solution_of_puzzle()
